e0c3f4849e12044c6822c863a755f238a992ba73
Patan Mahaboob Subhani <124454609+PatanMahaboobSubhani@users.noreply.github.com>
2025-06-05 13:38:27 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -8,7 +8,7 @@ const { getStyleDeclarationForSelector } = require('../test-utils/getStyleDeclar
 
 const { JSDOM } = require('jsdom');
 
-describe('CSS Flexbox', () => {
+describe('CSS Layout Basics', () => {
     let htmlString;
 
     let dom;
@@ -38,115 +38,119 @@ describe('CSS Flexbox', () => {
         expect(report).toEqual(expect.objectContaining({ valid: true }));
     });
 
-    describe('Fix invalid Flexbox properties', () => {
-        it('should fix flex-grow property in .card-fifth selector', async () => {
+    it('should set box-sizing properly', async() => {
+        await waitBrowserLoadEvent(document);
+        const styleDeclaration = getStyleDeclarationForSelector('body', document.styleSheets);
+        expect(styleDeclaration["box-sizing"]).toBe('border-box');
+    });
+
+    describe('Page skeleton', () => {
+        it('should make sidebar float', async() => {
             await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('.card-fifth', document.styleSheets);
-            expect(JSON.parse(styleDeclaration["flex-grow"])).toBe(4);
+            const styleDeclaration = getStyleDeclarationForSelector('aside', document.styleSheets);
+            expect(styleDeclaration["float"]).toBe('right');
         });
 
-        it('should fix flex-grow property in .card-six selector', async () => {
+        it('should make main float', async() => {
             await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('.card-six', document.styleSheets);
-            expect(JSON.parse(styleDeclaration["flex-grow"])).toBe(4);
+            const styleDeclaration = getStyleDeclarationForSelector('main', document.styleSheets);
+            expect(styleDeclaration["float"]).toBe('left');
         });
 
-        it('should fix display property in .header_line selector', async () => {
+        it('should make containers sizes according to schema', async() => {
             await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('.header_line', document.styleSheets);
-            expect(styleDeclaration["display"]).toBe("flex");
-        });
+            const mainWidth = getStyleDeclarationForSelector('main', document.styleSheets)["width"];
+            const headerWidth = getStyleDeclarationForSelector('header', document.styleSheets)["width"];
+            const footerWidth = getStyleDeclarationForSelector('footer', document.styleSheets)["width"];
+            const sideBarWidth = getStyleDeclarationForSelector('aside', document.styleSheets)["width"];
 
-        it('should fix align-items property in .header_line selector', async () => {
-            await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('.header_line', document.styleSheets);
-            expect(styleDeclaration["align-items"]).toBe("center");
-        });
-
-        it('should fix justify-content property in .title_section selector', async () => {
-            await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('.title_section', document.styleSheets);
-            expect(styleDeclaration["justify-content"]).toBe("center");
+            expect(mainWidth).toBe("70%");
+            expect(sideBarWidth).toBe("30%");
+            expect(headerWidth).toBe("100%");
+            expect(footerWidth).toBe("100%");
         });
     });
 
-    describe('Add proper CSS rules to required html elements', () => {
-        it('should add "display" property to nav ul', async () => {
+    describe('Header', () => {
+       it('should set proper position for .header_line', async() => {
             await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('nav ul', document.styleSheets);
-            expect(["flex", "inline-flex"].includes(styleDeclaration["display"])).toBe(true);
-        });
+            const headerPosition = getStyleDeclarationForSelector('.header_line', document.styleSheets)["position"];
+            
+            expect(headerPosition).toBe('fixed');
+       });
 
-        it('should add "flex-direction" property to .title_section', async () => {
+       it('should set .header_line at the top of viewport', async() => {
             await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('.title_section', document.styleSheets);
-            expect(styleDeclaration["flex-direction"]).toBe("row-reverse");
-        });
-
-        it('should make divs in Clientd section flexible', async () => {
-            await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('main section div', document.styleSheets);
-            expect(styleDeclaration["display"]).toBe("flex");
-        });
-
-        it('should apply flex-grow to .column', async () => {
-            await waitBrowserLoadEvent(document);
-            const commonStyleDeclaration = getStyleDeclarationForSelector('.column', document.styleSheets);
-            const specialStyleDeclaration = getStyleDeclarationForSelector('section:first-of-type div:first-of-type .column:first-of-type', document.styleSheets);
-            expect(commonStyleDeclaration["flex-grow"]).toBe("1");
-            expect(specialStyleDeclaration["flex-grow"]).toBe("2");
-        });
-
-        it('should fix order for .card selectors', async () => {
-            await waitBrowserLoadEvent(document);
-            const firstColStyleDeclaration = getStyleDeclarationForSelector('.card-first', document.styleSheets);
-            const secondColStyleDeclaration = getStyleDeclarationForSelector('.card-second', document.styleSheets);
-            const thirdColStyleDeclaration = getStyleDeclarationForSelector('.card-third', document.styleSheets);
-            const fourthColStyleDeclaration = getStyleDeclarationForSelector('.card-fouth', document.styleSheets);
-            const fifthColStyleDeclaration = getStyleDeclarationForSelector('.card-fifth', document.styleSheets);
-            const sixColStyleDeclaration = getStyleDeclarationForSelector('.card-six', document.styleSheets);
-
-            expect(firstColStyleDeclaration["order"]).toBe("1");
-            expect(secondColStyleDeclaration["order"]).toBe("2");
-            expect(thirdColStyleDeclaration["order"]).toBe("3");
-            expect(fourthColStyleDeclaration["order"]).toBe("4");
-            expect(fifthColStyleDeclaration["order"]).toBe("5");
-            expect(sixColStyleDeclaration["order"]).toBe("6");
-        });
-
-        it('should fix size of .card selectors', async () => {
-            await waitBrowserLoadEvent(document);
-            const firstColStyleDeclaration = getStyleDeclarationForSelector('.card-first', document.styleSheets);
-            const secondColStyleDeclaration = getStyleDeclarationForSelector('.card-second', document.styleSheets);
-            const thirdColStyleDeclaration = getStyleDeclarationForSelector('.card-third', document.styleSheets);
-            const fourthColStyleDeclaration = getStyleDeclarationForSelector('.card-fouth', document.styleSheets);
-
-            expect(firstColStyleDeclaration["flex-grow"]).toBe("1");
-            expect(secondColStyleDeclaration["flex-grow"]).toBe("1");
-            expect(thirdColStyleDeclaration["flex-grow"]).toBe("1");
-            expect(fourthColStyleDeclaration["flex-grow"]).toBe("1");
-        });
-
-        it('should implement styles for footer', async () => {
-            await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('footer', document.styleSheets);
-            expect(styleDeclaration["display"]).toBe("flex");
-            expect(styleDeclaration["justify-content"]).toBe("space-between");
-            expect(["end", "flex-end"].includes(styleDeclaration["align-items"])).toBe(true);
+            const headerPosition = getStyleDeclarationForSelector('.header_line', document.styleSheets)["top"];
+            
+            expect(headerPosition).toBe('0');
         });
     });
 
-    describe('Make it responsive', () => {
-        it("should make the flex items in menu always stay on one line", async () => {
+    describe('Main content', () => {
+        it('should split .client_stories into 3 columns', async() => {
             await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('nav ul', document.styleSheets);
-            expect(styleDeclaration["flex-wrap"]).toBe("nowrap");
+            const styleDeclaration = getStyleDeclarationForSelector('.client_stories', document.styleSheets);
+            
+            expect(styleDeclaration["column-count"]).toBe('3');
         });
 
-        it("should make .title_section to wrap in 2 rows for small screens", async () => {
+        it('should add gab between .client_stories columns', async() => {
             await waitBrowserLoadEvent(document);
-            const styleDeclaration = getStyleDeclarationForSelector('.title_section', document.styleSheets);
-            expect(styleDeclaration["flex-wrap"]).toBe("wrap");
+            const styleDeclaration = getStyleDeclarationForSelector('.client_stories', document.styleSheets);
+            
+            expect(styleDeclaration["column-gap"]).toBe('20px');
+        });
+
+        it('should make section heading to be proper width', async() => {
+            await waitBrowserLoadEvent(document);
+            const styleDeclaration = getStyleDeclarationForSelector('h2', document.styleSheets);
+            
+            expect(styleDeclaration["column-span"]).toBe('all');
+        });
+
+        it('should split .features into 2 columns', async() => {
+            await waitBrowserLoadEvent(document);
+            const styleDeclaration = getStyleDeclarationForSelector('.features', document.styleSheets);
+            
+            expect(styleDeclaration["column-count"]).toBe('2');
+        });
+
+        it('should remove gab between .features columns', async() => {
+            await waitBrowserLoadEvent(document);
+            const styleDeclaration = getStyleDeclarationForSelector('.features', document.styleSheets);
+            
+            expect(styleDeclaration["column-gap"]).toBe('0');
+        });
+    });
+
+    describe("Sidebar", () => {
+        it('should add overflow property for .info container', async() => {
+            await waitBrowserLoadEvent(document);
+            const styleDeclaration = getStyleDeclarationForSelector('.info', document.styleSheets);
+            
+            expect(styleDeclaration["overflow"]).toBe('auto');
+        });
+
+        it('should make `form` to apper at the bottom of `aside`', async() => {
+            await waitBrowserLoadEvent(document);
+            const formPosition = getStyleDeclarationForSelector('form', document.styleSheets)["position"];
+            const formPlacement = getStyleDeclarationForSelector('form', document.styleSheets);
+            const sideBarPosition = getStyleDeclarationForSelector('aside', document.styleSheets)["position"];
+            
+            expect(formPosition).toBe('absolute');
+            expect(sideBarPosition).toBe('relative');
+            expect(formPlacement["bottom"]).toBe('0');
+        });
+
+        it('should make overlapping `.header_line` cover `aside` when scroll occures', async() => {
+            await waitBrowserLoadEvent(document);
+            const headerStyleDeclaration = getStyleDeclarationForSelector('.header_line', document.styleSheets);
+            const sideBarStyleDeclaration = getStyleDeclarationForSelector('aside', document.styleSheets);
+            
+            expect(headerStyleDeclaration["z-index"]).not.toBe(undefined);
+            expect(sideBarStyleDeclaration["z-index"]).toBe(undefined);
         });
     });
 });
+
